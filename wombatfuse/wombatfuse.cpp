31608c85522b3fbea1b6f15422e3160308cca1ce
Pasquale Rinaldi <pjrinaldi@gmail.com>
2021-08-22 19:32:34 +0000

    wombatfuse almost working, ls works but size with -l option and content doesn't work either.

--- wombatfuse/wombatfuse.cpp
+++ wombatfuse/wombatfuse.cpp
@@ -31,7 +31,7 @@ static QString ifile;
 static QString mntpt;
 static const char* relativefilename = NULL;
 static const char* rawfilename = NULL;
-static qint64 totalbytes = 0;
+static quint64 totalbytes = 0;
 
 //static char* imgpath;
 
@@ -181,12 +181,14 @@ static int wombat_getattr(const char *path, struct stat *stbuf, struct fuse_file
             stbuf->st_size = (off_t)totalbytes;
             //stbuf->st_size = GetUncompressedSize();
 	}
+        /*
         else if(strcmp(path, rawfilename) == 0)
         {
             stbuf->st_mode = S_IFREG | 0444;
             stbuf->st_nlink = 1;
             stbuf->st_size = (off_t)totalbytes;
         }
+        */
         else
     	    res = -ENOENT;
 
@@ -234,10 +236,99 @@ static int wombat_read(const char *path, char *buf, size_t size, off_t offset, s
 	(void) fi;
         if(strcmp(path, relativefilename) != 0)
             return -ENOENT;
+
+    #define IN_CHUNK_SIZE  (16*1024)
+
+    char* cmpbuf = new char[IN_CHUNK_SIZE];
+    
+    LZ4F_dctx* lz4dctx;
+    LZ4F_frameInfo_t lz4frameinfo;
+    LZ4F_errorCode_t errcode;
+    
+    errcode = LZ4F_createDecompressionContext(&lz4dctx, LZ4F_getVersion());
+    if(LZ4F_isError(errcode))
+        printf("%s\n", LZ4F_getErrorName(errcode));
+
+    quint64 curpos = 0;
+    QFile cwfi(wfimg);
+    if(!cwfi.isOpen())
+	cwfi.open(QIODevice::ReadOnly);
+    QDataStream cin(&cwfi);
+    QFile rawdd(imgfile);
+    rawdd.open(QIODevice::WriteOnly);
+    QDataStream cout(&rawdd);
+
+    quint64 header;
+    uint8_t version;
+    QString cnum;
+    QString evidnum;
+    QString examiner2;
+    QString description2;
+    cin >> header >> version >> totalbytes >> cnum >> evidnum >> examiner2 >> description2;
+    if(header != 0x776f6d6261746669)
+    {
+        qDebug() << "Wrong file type, not a wombat forensic image.";
+        return -EIO;
+    }
+    if(version != 1)
+    {
+        qDebug() << "Not the correct wombat forensic image format.";
+        return -EIO;
+    }
+
+    int bytesread = cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
+    
+    size_t consumedsize = bytesread;
+    size_t framesize = LZ4F_getFrameInfo(lz4dctx, &lz4frameinfo, cmpbuf, &consumedsize);
+    if(LZ4F_isError(framesize))
+        printf("frameinfo error: %s\n", LZ4F_getErrorName(framesize));
+    
+    size_t rawbufsize = GetBlockSize(&lz4frameinfo);
+    char* rawbuf = new char[rawbufsize];
+    size_t filled = bytesread - consumedsize;
+    int firstchunk = 1;
+    size_t ret = 1;
+    while(ret != 0)
+    {
+        size_t readsize = firstchunk ? filled : cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
+        firstchunk = 0;
+        const void* srcptr = (const char*)cmpbuf + consumedsize;
+        consumedsize = 0;
+        const void* const srcend = (const char*)srcptr + readsize;
+        while(srcptr < srcend && ret != 0)
+        {
+            size_t dstsize = rawbufsize;
+            size_t srcsize = (const char*)srcend - (const char*)srcptr;
+            ret = LZ4F_decompress(lz4dctx, rawbuf, &dstsize, srcptr, &srcsize, NULL);
+            if(LZ4F_isError(ret))
+            {
+                printf("decompression error: %s\n", LZ4F_getErrorName(ret));
+            }
+            //write here
+            int byteswrote = cout.writeRawData(rawbuf, dstsize);
+            srcptr = (const char*)srcptr + srcsize;
+        }
+    }
+    //qDebug() << "ret should be zero:" << ret;
+
+    cwfi.close();
+    rawdd.close();
+    delete[] cmpbuf;
+    delete[] rawbuf;
+
+    errcode = LZ4F_freeDecompressionContext(lz4dctx);
+    rawdd.open(QIODevice::ReadOnly);
+    rawdd.seek(offset);
+    int byteswritten = rawdd.read(buf, size);
+    rawdd.close();
+
+    return byteswritten;
+
+
 //	if(strcmp(path+1, options.filename) != 0)
 //		return -ENOENT;
 
-        return Uncompress(buf, offset, size);
+        //return Uncompress(buf, offset, size);
         //len = 10;
 //	len = strlen(options.contents);
 	/*
