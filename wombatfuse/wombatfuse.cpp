71b77481b1b3644416e39e582e3984e2aa2e7e10
Pasquale Rinaldi <pjrinaldi@gmail.com>
2021-08-23 23:23:55 +0000

    working on seek/read for uncompressed data...

--- wombatfuse/wombatfuse.cpp
+++ wombatfuse/wombatfuse.cpp
@@ -38,7 +38,6 @@ static FILE* infile = NULL;
 static off_t lz4size = 0;
 static off_t rawsize = 0;
 
-/*
 static size_t GetBlockSize(const LZ4F_frameInfo_t* info)
 {
     switch (info->blockSizeID)
@@ -54,6 +53,7 @@ static size_t GetBlockSize(const LZ4F_frameInfo_t* info)
     }
 }
 
+/*
 qint64 Uncompress(char* buf, off_t offset, size_t size)
 {
     #define IN_CHUNK_SIZE  (16*1024)
@@ -210,9 +210,94 @@ static int wombat_read(const char *path, char *buf, size_t size, off_t offset, s
     if(strcmp(path, relativefilename) != 0)
         return -ENOENT;
 
-    fseek(infile, offset, SEEK_SET);
-    int insize = fread(buf, 1, size, infile);
-    printf("insize: %d\n", insize);
+    #define IN_CHUNK_SIZE  (16*1024)
+
+    char* cmpbuf = new char[IN_CHUNK_SIZE];
+    
+    LZ4F_dctx* lz4dctx;
+    LZ4F_frameInfo_t lz4frameinfo;
+    LZ4F_errorCode_t errcode;
+    
+    errcode = LZ4F_createDecompressionContext(&lz4dctx, LZ4F_getVersion());
+    if(LZ4F_isError(errcode))
+        printf("%s\n", LZ4F_getErrorName(errcode));
+
+    quint64 curpos = 0;
+    QFile cwfi(wfimg);
+    if(!cwfi.isOpen())
+	cwfi.open(QIODevice::ReadOnly);
+    QDataStream cin(&cwfi);
+    //QFile rawdd(imgfile);
+    //rawdd.open(QIODevice::WriteOnly);
+    //QDataStream cout(&rawdd);
+
+    quint64 header;
+    uint8_t version;
+    QString cnum;
+    qint64 totalbytes;
+    QString evidnum;
+    QString examiner2;
+    QString description2;
+    cin >> header >> version >> totalbytes >> cnum >> evidnum >> examiner2 >> description2;
+    if(header != 0x776f6d6261746669)
+    {
+        qDebug() << "Wrong file type, not a wombat forensic image.";
+        return -EIO;
+    }
+    if(version != 1)
+    {
+        qDebug() << "Not the correct wombat forensic image format.";
+        return -EIO;
+    }
+
+    int bytesread = cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
+    
+    size_t consumedsize = bytesread;
+    size_t framesize = LZ4F_getFrameInfo(lz4dctx, &lz4frameinfo, cmpbuf, &consumedsize);
+    if(LZ4F_isError(framesize))
+        printf("frameinfo error: %s\n", LZ4F_getErrorName(framesize));
+    
+    size_t rawbufsize = GetBlockSize(&lz4frameinfo);
+    char* rawbuf = new char[rawbufsize];
+    size_t filled = bytesread - consumedsize;
+    int firstchunk = 1;
+    size_t ret = 1;
+    while(ret != 0)
+    {
+        size_t readsize = firstchunk ? filled : cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
+        firstchunk = 0;
+        const void* srcptr = (const char*)cmpbuf + consumedsize;
+        consumedsize = 0;
+        const void* const srcend = (const char*)srcptr + readsize;
+        while(srcptr < srcend && ret != 0)
+        {
+            size_t dstsize = rawbufsize;
+            size_t srcsize = (const char*)srcend - (const char*)srcptr;
+            ret = LZ4F_decompress(lz4dctx, rawbuf, &dstsize, srcptr, &srcsize, NULL);
+            if(LZ4F_isError(ret))
+            {
+                printf("decompression error: %s\n", LZ4F_getErrorName(ret));
+            }
+            //write here
+            int byteswrote = cout.writeRawData(rawbuf, dstsize);
+            srcptr = (const char*)srcptr + srcsize;
+        }
+    }
+    //qDebug() << "ret should be zero:" << ret;
+
+    cwfi.close();
+    //rawdd.close();
+    delete[] cmpbuf;
+    delete[] rawbuf;
+
+    errcode = LZ4F_freeDecompressionContext(lz4dctx);
+    //rawdd.open(QIODevice::ReadOnly);
+    //rawdd.seek(offset);
+    //int byteswritten = rawdd.read(buf, size);
+    //rawdd.close();
+    //fseek(infile, offset, SEEK_SET);
+    //int insize = fread(buf, 1, size, infile);
+    //printf("insize: %d\n", insize);
 
     return insize;
 }
@@ -259,10 +344,9 @@ int main(int argc, char* argv[])
     ifile = "/" + imgfile;
     relativefilename = ifile.toStdString().c_str();
     rawfilename = imgfile.toStdString().c_str();
-    //qDebug() << wfimg << imgfile << ifile;
 
     lz4filename = wfimg.toStdString();
-    printf("lz4filename: %s\n", lz4filename.c_str());
+    //printf("lz4filename: %s\n", lz4filename.c_str());
     infile = fopen(lz4filename.c_str(), "rb");
     if(infile != NULL)
     {
@@ -273,7 +357,7 @@ int main(int argc, char* argv[])
         printf("file failed to open\n");
     fseek(infile, 0, SEEK_SET);
 
-    printf("infile size: %ld\n", lz4size);
+    //printf("infile size: %ld\n", lz4size);
 
     QFile cwfile(wfimg);
     cwfile.open(QIODevice::ReadOnly);
