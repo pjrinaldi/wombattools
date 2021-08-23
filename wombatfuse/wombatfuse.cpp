2f63635d431ff0cf4b7ce5a38e1e2939ead6b2dc
Pasquale Rinaldi <pjrinaldi@gmail.com>
2021-08-23 00:32:44 +0000

    continuing to simplify the code to get it to work, but no dice yet.

--- wombatfuse/wombatfuse.cpp
+++ wombatfuse/wombatfuse.cpp
@@ -39,6 +39,7 @@ static off_t lz4size = 0;
 
 //static char* imgpath;
 
+/*
 static size_t GetBlockSize(const LZ4F_frameInfo_t* info)
 {
     switch (info->blockSizeID)
@@ -143,6 +144,7 @@ qint64 Uncompress(char* buf, off_t offset, size_t size)
 
     return size;
 }
+*/
 
 static void *wombat_init(struct fuse_conn_info *conn, struct fuse_config *cfg)
 {
@@ -156,22 +158,6 @@ static int wombat_getattr(const char *path, struct stat *stbuf, struct fuse_file
 	(void) fi;
 	int res = 0;
 
-        /*
-        QFile wfi(wfimg);
-        if(!wfi.isOpen())
-            wfi.open(QIODevice::ReadOnly);
-        QDataStream in(&wfi);
-        quint64 header;
-        uint8_t version;
-        qint64 totalbytes;
-        QString casenumber;
-        QString evidnumber;
-        QString examiner;
-        QString description;
-        in >> header >> version >> totalbytes >> casenumber >> evidnumber >> examiner >> description;
-        wfi.close();
-        */
-
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0)
         {
@@ -183,19 +169,7 @@ static int wombat_getattr(const char *path, struct stat *stbuf, struct fuse_file
             stbuf->st_mode = S_IFREG | 0444;
             stbuf->st_nlink = 1;
             stbuf->st_size = lz4size;
-            //stbuf->st_size = (off_t)totalbytes;
-            //QFileInfo wfi(wfimg);
-            //stbuf->st_size = (off_t)wfi.size();
-            //stbuf->st_size = GetUncompressedSize();
 	}
-        /*
-        else if(strcmp(path, rawfilename) == 0)
-        {
-            stbuf->st_mode = S_IFREG | 0444;
-            stbuf->st_nlink = 1;
-            stbuf->st_size = (off_t)totalbytes;
-        }
-        */
         else
     	    res = -ENOENT;
 
@@ -207,9 +181,6 @@ static int wombat_readdir(const char *path, void *buf, fuse_fill_dir_t filler, o
 	(void) offset;
 	(void) fi;
 	(void) flags;
-
-        //printf("path: %s\n", path); 
-        //qDebug() << "path:" << path;
 
 	if (strcmp(path, "/") != 0)
 		return -ENOENT;
@@ -223,117 +194,26 @@ static int wombat_readdir(const char *path, void *buf, fuse_fill_dir_t filler, o
 
 static int wombat_open(const char *path, struct fuse_file_info *fi)
 {
-        //qDebug() << "path:" << path;
         if(strcmp(path, relativefilename) != 0)
                 return -ENOENT;
         if(strcmp(path, rawfilename) != 0)
             return -ENOENT;
-	//if (strcmp(path+1, options.filename) != 0)
-	//	return -ENOENT;
 
 	if ((fi->flags & O_ACCMODE) != O_RDONLY)
 		return -EACCES;
-
 
 	return 0;
 }
 
 static int wombat_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
-	//size_t len;
-	(void) fi;
-        if(strcmp(path, relativefilename) != 0)
-            return -ENOENT;
+    (void) fi;
+    if(strcmp(path, relativefilename) != 0)
+        return -ENOENT;
 
-    #define IN_CHUNK_SIZE  (16*1024)
-
-    char* cmpbuf = new char[IN_CHUNK_SIZE];
-    
-    LZ4F_dctx* lz4dctx;
-    LZ4F_frameInfo_t lz4frameinfo;
-    LZ4F_errorCode_t errcode;
-    
-    errcode = LZ4F_createDecompressionContext(&lz4dctx, LZ4F_getVersion());
-    if(LZ4F_isError(errcode))
-        printf("%s\n", LZ4F_getErrorName(errcode));
-
-    quint64 curpos = 0;
-    QFile cwfi(wfimg);
-    if(!cwfi.isOpen())
-	cwfi.open(QIODevice::ReadOnly);
-    QDataStream cin(&cwfi);
-    QFile rawdd(imgfile);
-    rawdd.open(QIODevice::WriteOnly);
-    QDataStream cout(&rawdd);
-
-    quint64 header;
-    uint8_t version;
-    QString cnum;
-    QString evidnum;
-    QString examiner2;
-    QString description2;
-    cin >> header >> version >> totalbytes >> cnum >> evidnum >> examiner2 >> description2;
-    if(header != 0x776f6d6261746669)
-    {
-        qDebug() << "Wrong file type, not a wombat forensic image.";
-        return -EIO;
-    }
-    if(version != 1)
-    {
-        qDebug() << "Not the correct wombat forensic image format.";
-        return -EIO;
-    }
-
-    int bytesread = cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
-    
-    size_t consumedsize = bytesread;
-    size_t framesize = LZ4F_getFrameInfo(lz4dctx, &lz4frameinfo, cmpbuf, &consumedsize);
-    if(LZ4F_isError(framesize))
-        printf("frameinfo error: %s\n", LZ4F_getErrorName(framesize));
-    
-    size_t rawbufsize = GetBlockSize(&lz4frameinfo);
-    char* rawbuf = new char[rawbufsize];
-    size_t filled = bytesread - consumedsize;
-    int firstchunk = 1;
-    size_t ret = 1;
-    while(ret != 0)
-    {
-        size_t readsize = firstchunk ? filled : cin.readRawData(cmpbuf, IN_CHUNK_SIZE);
-        firstchunk = 0;
-        const void* srcptr = (const char*)cmpbuf + consumedsize;
-        consumedsize = 0;
-        const void* const srcend = (const char*)srcptr + readsize;
-        while(srcptr < srcend && ret != 0)
-        {
-            size_t dstsize = rawbufsize;
-            size_t srcsize = (const char*)srcend - (const char*)srcptr;
-            ret = LZ4F_decompress(lz4dctx, rawbuf, &dstsize, srcptr, &srcsize, NULL);
-            if(LZ4F_isError(ret))
-            {
-                printf("decompression error: %s\n", LZ4F_getErrorName(ret));
-            }
-            //write here
-            int byteswrote = cout.writeRawData(rawbuf, dstsize);
-            srcptr = (const char*)srcptr + srcsize;
-        }
-    }
-    //qDebug() << "ret should be zero:" << ret;
-
-    cwfi.close();
-    rawdd.close();
-    delete[] cmpbuf;
-    delete[] rawbuf;
-
-    errcode = LZ4F_freeDecompressionContext(lz4dctx);
-    /*
-    rawdd.open(QIODevice::ReadOnly);
-    rawdd.seek(offset);
-    int byteswritten = rawdd.read(buf, size);
-    rawdd.close();
-    */
-    FILE* rawf = fopen(rawfilename,"rb");
-    fseek(rawf, offset, SEEK_SET);
-    int insize = fread(buf, 1, size, rawf);
+    fseek(infile, offset, SEEK_SET);
+    int insize = fread(buf, 1, size, infile);
+    printf("insize: %d\n", insize);
 
     return insize;
 
@@ -341,7 +221,6 @@ static int wombat_read(const char *path, char *buf, size_t size, off_t offset, s
 //	if(strcmp(path+1, options.filename) != 0)
 //		return -ENOENT;
 
-        //return Uncompress(buf, offset, size);
         //len = 10;
 //	len = strlen(options.contents);
 	/*
@@ -400,7 +279,7 @@ int main(int argc, char* argv[])
     ifile = "/" + imgfile;
     relativefilename = ifile.toStdString().c_str();
     rawfilename = imgfile.toStdString().c_str();
-    qDebug() << wfimg << imgfile << ifile;
+    //qDebug() << wfimg << imgfile << ifile;
 
     lz4filename = wfimg.toStdString();
     printf("lz4filename: %s\n", lz4filename.c_str());
@@ -412,23 +291,10 @@ int main(int argc, char* argv[])
     }
     else
         printf("file failed to open\n");
+    fseek(infile, 0, SEEK_SET);
 
     printf("infile size: %ld\n", lz4size);
-    QFile wfi(wfimg);
-    if(!wfi.isOpen())
-        wfi.open(QIODevice::ReadOnly);
-    QDataStream in(&wfi);
-    quint64 header;
-    uint8_t version;
-    qint64 totalbytes;
-    QString casenumber;
-    QString evidnumber;
-    QString examiner;
-    QString description;
-    in >> header >> version >> totalbytes >> casenumber >> evidnumber >> examiner >> description;
-    wfi.close();
 
-    qDebug() << "totalbytes:" << totalbytes << (off_t)totalbytes;
     char** fargv = NULL;
     fargv = (char**)calloc(2, sizeof(char*));
     int fargc = 2;
@@ -440,6 +306,7 @@ int main(int argc, char* argv[])
 
     //fuse_opt_parse(NULL, NULL, NULL, NULL);
     ret = fuse_main(fuseargs.argc, fuseargs.argv, &wombat_oper, NULL);
+    
     fuse_opt_free_args(&fuseargs);
 
     return ret;
