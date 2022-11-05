c87b451f14d7600a4a0050380b68f3fa27cfe042
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-11-05 18:19:17 +0000

    updated build script to remove wombatlogical until it is ready and then updated the script to allow for the installation of the tools to /usr/local/bin

--- build.sh
+++ build.sh
@@ -8,23 +8,35 @@ echo "Building wombatinfo"
 
 gcc -O3 -o wombatinfo wombatinfo.c
 
+echo "Install wombatinfo (root)"
+
+sudo cp wombatinfo /usr/local/bin/
+
 echo "Building wombatverify"
 	
 gcc -O3 -o wombatverify wombatverify.c blake3.c blake3_dispatch.c blake3_portable.c blake3_sse2_x86-64_unix.S blake3_sse41_x86-64_unix.S blake3_avx2_x86-64_unix.S blake3_avx512_x86-64_unix.S -lzstd
+
+echo "Install wombatverify (root)"
+
+sudo cp wombatverify /usr/local/bin/
 
 echo "Building wombatmount"
 
 gcc -O3 -o wombatmount wombatfuse.c -I/usr/include/fuse3 -lfuse3 -lpthread -lzstd
 
+echo "Install wombatmount (root)"
+
+sudo cp wombatmount /usr/local/bin/
+
 echo "Building libblake3.so"
 
 gcc -shared -O3 -o libblake3.so blake3.c blake3_dispatch.c blake3_portable.c blake3_sse2_x86-64_unix.S blake3_sse41_x86-64_unix.S blake3_avx2_x86-64_unix.S blake3_avx512_x86-64_unix.S
 
-echo "copy libblake3.so to user lib as root"
+echo "Install libblake3.so to /usr/local/lib (root)"
 
 sudo cp libblake3.so /usr/local/lib/
 
-echo "reload library config as root"
+echo "reload library config (root)"
 
 sudo ldconfig
 
@@ -41,16 +53,20 @@ echo "Building wombatimager"
 g++ -O3 -o wombatimager wombatimager.cpp -lzstd -ludev -lpthread -L. -lblake3
 #g++ -O3 -static -o wombatimager wombatimager.cpp -lzstd -ludev -lpthread -L. -lblake3
 
+echo "Install wombatimager (root)"
+
+sudo cp wombatimager /usr/local/bin/
+
 echo "Building wombatrestore"
 
 g++ -O3 -o wombatrestore wombatrestore.cpp -lzstd -lpthread -L. -lblake3
 
-echo "Building wombatlogical"
+echo "Install wombatrestore (root)"
 
-g++ -O3 -o wombatlogical wombatlogical.cpp -ltar -lzstd -lpthread -L. -lblake3
+sudo cp wombatrestore /usr/local/bin/
 
-# Moved to it's own repository
-#echo "Building wombatfileforensics"
+# Not ready for use yet...
+#echo "Building wombatlogical"
 
-#g++ -O3 -o wombatfileforensics common.cpp ntfs.cpp fatfs.cpp extfs.cpp wombatfileforensics.cpp -lpthread -L. -lblake3
+#g++ -O3 -o wombatlogical wombatlogical.cpp -ltar -lzstd -lpthread -L. -lblake3
 
